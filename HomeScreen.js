7f9ecdf46e366b1f787e12effaac1e590bc3bf90
edwincarbajal <exaviercarbajal@gmail.com>
2019-11-10 09:00:07 -0500

    Add google cloud vision api call

--- HomeScreen.js
+++ HomeScreen.js
@@ -4,18 +4,13 @@ import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
 
 export default class HomeScreen extends Component {
-  // render() {
-  //   return(
-  //     <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-  //       <Text>Home Screen</Text>
-  //       <Text>Camera Component goes here :)</Text>
-  //     </SafeAreaView>
-  //   )
   constructor(props) {
     super(props);
     this.state = {
       hasCameraPermission: null,
       type: Camera.Constants.Type.back,
+      base64: '',
+      googleResponse: null
     };
   }
 
@@ -32,8 +27,12 @@ export default class HomeScreen extends Component {
       return <Text>No access to camera</Text>;
     } else {
       return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <Camera
+            ref={ref => { this.camera = ref; }}
+            style={{ flex: 1 }}
+            type={this.state.type}
+          >
             <View
               style={{
                 flex: 1,
@@ -46,19 +45,56 @@ export default class HomeScreen extends Component {
                   alignSelf: 'flex-end',
                   alignItems: 'center',
                 }}
-                onPress={() => {
-                  this.setState({
-                    type:
-                      this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back,
-                  });
+                onPress={async () => {
+                  if (this.camera) {
+                    let photo = await this.camera.takePictureAsync({base64: true});
+                    this.setState({ base64: photo.base64 })
+
+                    try {
+                      this.setState({ uploading: true });
+                      let { base64 } = this.state;
+                      let body = JSON.stringify({
+                        requests: [
+                          {
+                            'features': [
+                              {
+                                'type': 'LOGO_DETECTION',
+                                "maxResults": 1,
+                              },
+                            ],
+                            "image": {
+                              "content": base64
+                            },
+                          }
+                        ]
+                      });
+                      let response = await fetch(
+                        `https://vision.googleapis.com/v1/images:annotate?key=${googleCloudApiKey}`,
+                        {
+                          headers: {
+                            Accept: "application/json",
+                            "Content-Type": "application/json"
+                          },
+                          method: "POST",
+                          body: body
+                        }
+                      );
+                      let responseJson = await response.json();
+                      console.log(responseJson);
+                      this.setState({
+                        googleResponse: responseJson,
+                        uploading: false
+                      });
+                    } catch (error) {
+                      console.log(error);
+                    }
+                  };
                 }}>
-                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Take Photo </Text>
               </TouchableOpacity>
             </View>
           </Camera>
-        </View>
+        </SafeAreaView>
       );
     }
   }
